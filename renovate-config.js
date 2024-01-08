675d5d764e325781dbeadb112152b86222b9017e
Till Adam <till.adam@dm.de>
2024-01-08 10:08:33 +0100

    chore: renovate config

---
+++ renovate-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    platform: "gitlab",
+    token: process.env.RENOVATE_TOKEN,
+    extends: [
+      ":semanticPrefixFixDepsChoreOthers",
+      ":ignoreModulesAndTests",
+      ":autodetectRangeStrategy",
+      "group:monorepos",
+      "group:recommended",
+      "workarounds:all",
+    ],
+    enabledManagers: ["regex"],
+    repositories: ["TheKangaroo/renovate-changelog"],
+    regexManagers: [
+        {
+          fileMatch: ["(^|\\/|\\.)(hcl|yaml)$"],
+          matchStrings: [
+            '# renovate: datasource=(?<datasource>.*?) depName=(?<depName>.*?) versioning=(?<versioning>.*?)\\s.*?_version:.*"(?<currentValue>.*)"\\s',
+          ],
+        },
+      ],
+
+  };
